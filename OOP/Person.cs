f1599f5f9c4700e2e0073c23b823b7c62ba6ef73
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-08 11:56:08 +0100

    Add project files.

---
+++ OOP/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP
+{
+    internal class Person
+    {
+        public string Name { get; set; } = string.Empty;
+        //plus 10 saker till
+    }
+
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+
+    internal class Admin : Employee
+    {
+        public string Department { get; set; }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Kalle";
+
+            Employee employee = new Employee();
+            employee.Salary = 25;
+            employee.Name = "Anna";
+
+            Person employee2 = new Employee() { Salary = 34000 };
+            employee2.Name = "Owe";
+
+
+
+            Admin admin = new Admin();
+            admin.Department = "IT";
+
+            Employee admin2 = new Admin() { Department = "finance" };
+            Admin castedAdmin = (Admin)admin2;
+            Console.WriteLine(castedAdmin.Department);
+            
+            Person admin3 = new Admin();
+            
+
+
+            var list = new List<Person>()
+            {
+                person, employee, admin2, admin
+            };
+            //list.Add(person);
+            //list.Add(employee2);
+            //list.Add(employee);
+            //list.Add(admin);
+
+
+
+
+
+           
+        }
+    }
+}
