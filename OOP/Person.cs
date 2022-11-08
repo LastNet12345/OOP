85fda0f5c950cab9a7494dff3ca82b0912e0aa3c
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2022-11-08 13:26:58 +0100

    Virtual Overide

--- OOP/Person.cs
+++ OOP/Person.cs
@@ -1,18 +1,39 @@
 ﻿namespace OOP
 {
-    internal class Person
+
+
+    internal interface IPerson
+    {
+        public string Name { get; set; }
+        void Do();
+        
+    } 
+    
+    internal class Person : IPerson
     {
         public string Name { get; set; } = string.Empty;
         //plus 10 saker till
+
+        public virtual void Do()
+        {
+            Console.WriteLine("Person doing");
+        }
     }
 
     internal class Employee : Person
     {
         public int Salary { get; set; }
+        
     }
 
     internal class Admin : Employee
     {
         public string Department { get; set; }
+
+        public override void Do()
+        {
+           // base.Do();
+            Console.WriteLine("Admin doing");
+        }
     }
 }
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -10,6 +10,7 @@
             Employee employee = new Employee();
             employee.Salary = 25;
             employee.Name = "Anna";
+            employee.Do();
 
             Person employee2 = new Employee() { Salary = 34000 };
             employee2.Name = "Owe";
@@ -22,12 +23,14 @@
             Employee admin2 = new Admin() { Department = "finance" };
             Admin castedAdmin = (Admin)admin2;
             Console.WriteLine(castedAdmin.Department);
+            castedAdmin.Do();
             
             Person admin3 = new Admin();
+            IPerson admin4 = new Admin();
             
 
 
-            var list = new List<Person>()
+            var list = new List<IPerson>()
             {
                 person, employee, admin2, admin
             };
@@ -35,6 +38,11 @@
             //list.Add(employee2);
             //list.Add(employee);
             //list.Add(admin);
+
+            foreach(Person person1 in list)
+            {
+                person1.Do();
+            }
 
 
 
